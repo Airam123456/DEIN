3439040b31b8d78edaacf633c4532a84fbb0c524
AiramBrTr <perseo_39@hotmail.es>
2022-10-13 08:58:56 +0200

    Mirar el git de Mario

--- EjercicioH/src/module-info.java
+++ EjercicioH/src/module-info.java
@@ -6,8 +6,11 @@ module EjercicioH {
 	requires javafx.swing;
 	requires javafx.media;
 	requires javafx.graphics;
+	requires java.sql;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens controller to javafx.graphics, javafx.fxml;
 	opens model to javafx.base, javafx.fxml;
+	opens dao to javafx.base;
+	
 }
