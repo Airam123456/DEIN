1ec7ba0763c86e4a0c0b11c30c233f1555781e62
AiramBrTr <perseo_39@hotmail.es>
2022-10-25 09:59:02 +0200

    EMPEZAMOS PROYECTO

---
+++ Proyecto1/src/module-info.java
@@ -0,0 +1,16 @@
+module Proyecto1 {
+
+	requires javafx.controls;
+	requires java.desktop;
+	requires javafx.web;
+	requires javafx.fxml;
+	requires javafx.swing;
+	requires javafx.media;
+	requires javafx.graphics;
+	requires java.sql;
+
+	opens application to javafx.graphics, javafx.fxml;
+	opens controller to javafx.graphics, javafx.fxml;
+	// opens model to javafx.base, javafx.fxml;
+	// opens dao to javafx.base;
+}
