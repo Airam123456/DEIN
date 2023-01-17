6b7e4fec08cdcdc983d9d0ceb8bc6e195df3bc7e
AiramBrTr <perseo_39@hotmail.es>
2023-01-17 09:52:40 +0100

    Proyecto

--- Proyecto2/src/application/Main.java
+++ Proyecto2/src/application/Main.java
@@ -3,6 +3,8 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.fxml.FXMLLoader;
 
@@ -11,12 +13,20 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			GridPane root = (GridPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			GridPane root = (GridPane)FXMLLoader.load(getClass().getResource("/fxml/Biblioteca.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("/css/application.css").toExternalForm());
+			primaryStage.setTitle("BIBLIOTECA");
+			Image imagen = new Image(getClass().getResource("/picture/sports.jpg").toString());
+			primaryStage.getIcons().add(imagen);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
+			Alert alert = new Alert(Alert.AlertType.ERROR);
+			alert.setHeaderText(null);
+			alert.setTitle("Error");
+			alert.setContentText("Error de ejecucion");
+			alert.showAndWait();
 			e.printStackTrace();
 		}
 	}
--- Proyecto2/src/module-info.java
+++ Proyecto2/src/module-info.java
@@ -1,6 +1,17 @@
 module Proyecto2 {
 	requires javafx.controls;
+	requires java.desktop;
+	requires javafx.web;
 	requires javafx.fxml;
-	
+	requires javafx.swing;
+	requires javafx.media;
+	requires javafx.graphics;
+	requires java.sql;
+	requires javafx.base;
+	requires org.kordamp.ikonli.javafx;
+
 	opens application to javafx.graphics, javafx.fxml;
+//	opens controller to javafx.graphics, javafx.fxml;
+	opens model to javafx.base, javafx.fxml;
+//	opens dao to javafx.base;
 }
