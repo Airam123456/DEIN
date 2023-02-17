464e9ef8a5df51b654c2bd0c788c9f2bf0556911
AiramBrTr <perseo_39@hotmail.es>
2023-02-17 09:59:14 +0100

    n hecho

---
+++ EjercicioN/src/module-info.java
@@ -0,0 +1,7 @@
+module EjercicioN {
+	requires javafx.controls;
+	requires javafx.fxml;
+	
+	opens application to javafx.graphics, javafx.fxml;
+	opens controller to javafx.graphics, javafx.fxml;
+}
---
+++ EjercicioO/src/module-info.java
@@ -0,0 +1,6 @@
+module EjercicioO {
+	requires javafx.controls;
+	requires javafx.fxml;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
